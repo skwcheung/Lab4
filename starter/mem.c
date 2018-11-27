65fa81d2f64921e425f37888a96f379cba937593
Steven Cheung <skwcheung@v1020-wn-126-34.campus-dynamic.uwaterloo.ca>
2018-11-27 16:25:45 -0500

    Started mem init and mem alloc for worst fit

--- starter/mem.c
+++ starter/mem.c
@@ -9,6 +9,7 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include "mem.h"
+#include<stdint.h>
 
 /* defines */
 
@@ -16,31 +17,68 @@
 void* best_fit_mem_start;
 void* worst_fit_mem_start;
 
-
 typedef struct node{
-	node* next_node;
-	node* prev_node;
-	int mem_size;
+	struct node* next_node;
+	struct node* prev_node;
 	void* mem_start;
-}node;
+	size_t mem_size;
+	unsigned int filled;
+}node_t;
+
+typedef struct head{
+	struct node* first_node;
+	void* end_of_mem;
+}head_t;
+
+void worst_fit_create_node(node_t* node, node_t* new_node){
+	head_t* head = worst_fit_mem_start;
+	new_node -> next_node = node -> next_node;
+	new_node -> prev_node = node;
+	new_node -> filled = 0;
+	if(node -> next_node == NULL)
+		new_node -> mem_size = ((intptr_t)head -> end_of_mem) - ((intptr_t)new_node + sizeof(node_t));
+	else
+		new_node -> mem_size = (new_node -> next_node) - ((intptr_t)new_node + sizeof(node_t));		
+		
+	int offset_four_byte = ((intptr_t)new_node + sizeof(node_t)) % 4;		
+	new_node -> mem_start = (intptr_t)new_node + sizeof(node_t) + offset_four_byte;
+	new_node -> mem_size -= offset_four_byte;
+	node -> next_node = new_node;	
+}
 
 /* Functions */
 
 /* memory initializer */
 int best_fit_memory_init(size_t size)
 {
-
-	// To be completed by students
-	// You call malloc once here to obtain the memory to be managed.
+	best_fit_mem_start = malloc(size);
 	return 0;
 
 }
 
 int worst_fit_memory_init(size_t size)
 {
+	if(size < (sizeof(node_t) + sizeof(head_t))){
+		printf("Error: Size too small");
+		return 0;
+	}
+		
+	
+	worst_fit_mem_start = malloc(size);
+	printf("Start of memory is located at %x \n",worst_fit_mem_start);
+	head_t* head = worst_fit_mem_start;
+	head->first_node = sizeof(head_t) + worst_fit_mem_start;
+	head->end_of_mem = worst_fit_mem_start + size;
+	printf("End of memory is located at %x \n",head->end_of_mem);
+	node_t* first_node = head->first_node;
+	first_node -> next_node = NULL;
+	first_node -> prev_node = NULL;
+	first_node -> mem_start = first_node + sizeof(node_t);
+	first_node -> mem_size = size - (sizeof(head_t) + sizeof(node_t));
+	first_node -> filled = 0;
 
-	// To be completed by students
-	// You call malloc once here to obtain the memory to be managed.
+	printf("First memory location available at %x with space %d \n",first_node->mem_start,first_node->mem_size);
+	printf("Size of head %d and size of node %d",sizeof(head_t),sizeof(node_t));
 	return 0;
 
 }
@@ -48,14 +86,55 @@ int worst_fit_memory_init(size_t size)
 /* memory allocators */
 void *best_fit_alloc(size_t size)
 {
-	// To be completed by students
+	
 	return NULL;
 }
 
 
 void *worst_fit_alloc(size_t size)
 {
-	// To be completed by students
+	node_t* new_node;
+	node_t* largest_available = NULL;
+	size_t largest_size = -1;
+
+	head_t* head = worst_fit_mem_start;
+	node_t* itr = head->first_node;
+
+	// Check for first entry aka base case
+	if(itr->next_node == NULL){
+		if(size > (itr->mem_size + sizeof(node_t))){
+			//error
+			return 1;
+		}
+		itr->next_node = itr + size + sizeof(node_t);
+		itr->mem_start = itr + sizeof(node_t);
+		itr->mem_size = size;
+		itr->filled = 1;
+
+		new_node = itr->next_node;
+		new_node->next_node = NULL;
+		new_node->prev_node = itr;
+		new_node->mem_start = new_node + sizeof(node_t);
+		new_node->mem_size = head->end_of_mem - new_node->mem_start;
+		new_node->filled = 0;
+	}
+
+	// Loop to search for largest available memory location
+	while(itr != NULL){
+		if(itr->filled == 0 && itr->mem_size > largest_size){
+			largest_available = itr;
+			largest_size = itr->mem_size;
+		}
+	}
+
+	// If available space of sufficient size was found
+	if(largest_size > size + sizeof(node_t)){
+		itr = largest_available; //Focus itr onto largest available node
+		node_t* new_node = (intptr_t)itr + sizeof(node_t) + size;
+		worst_fit_create_node(itr,new_node);
+		return itr->mem_start;
+	}
+
 	return NULL;
 }
 
