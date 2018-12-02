fd3012dbdc0580f22afd2453b361c1112e3aa260
Joseph Raymond Groothuizen <jrgrooth@edu.uwaterloo.ca>
2018-12-02 09:45:37 -0500

    Added Tests
    Added i some test s in getting issue with seg fault fouid issue in combine nodes

--- starter/main_test.c
+++ starter/main_test.c
@@ -9,81 +9,370 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 /* non-system provided header files. 
    Do not include more user-defined header files here
  */
 #include "mem.h"
+enum test_type {best_fit, worst_fit};
+
+void clear_mem(void** pointers, int length, enum test_type best_or_worst){
+	int i = 0;
+	for(; i < length; ++i){
+		if(best_or_worst == best_fit){
+			best_fit_dealloc(pointers[i]);
+		}
+		else{
+			worst_fit_dealloc(pointers[i]);
+		}
+	}
+}
+
+//allocate then deallocate same memory ensure pointer doesn't change
+void test_1(void** pointers, enum test_type best_or_worst){
+	int i = 0;
+	void* allocated;
+	pointers[0] = NULL;
+	for(; i < 10; ++i){
+		switch(best_or_worst){
+			case best_fit: 
+				allocated = best_fit_alloc(4);
+				//printf("allocated %x old %x\n", allocated, pointers[0]);
+				if(i != 0 && allocated != pointers[0]){
+					printf("ERROR\n");
+				}
+				pointers[0] = allocated;
+				//printf("before dealloc\n");
+				//print_mem(0);
+				best_fit_dealloc(allocated);
+				//printf("after dealloc\n");
+				break;
+			case worst_fit:
+				allocated = worst_fit_alloc(4);
+				//printf("allocated %x old %x\n", allocated, pointers[0]);
+				if(i != 0 && allocated != pointers[0]){
+					printf("ERROR\n");
+				}
+				pointers[0] = allocated;
+				//printf("before dealloc\n");
+				worst_fit_dealloc(allocated);
+				//printf("after dealloc\n");
+				break;
+		}
+	}
+}
+
+//allocate 10 space deallocate all and ensure no memory fragmentation
+void test_2(void** pointers, enum test_type best_or_worst){
+	int i = 0;
+	int fragmentation;
+	for(; i < 10; ++i){
+		switch(best_or_worst){
+			case best_fit:
+				pointers[i] = best_fit_alloc(4);
+				break;
+			case worst_fit:
+				pointers[i] = worst_fit_alloc(4);
+				break;
+		}
+	}
+	clear_mem(pointers, 10, best_or_worst);
+	switch(best_or_worst){
+		case best_fit:
+			fragmentation = best_fit_count_extfrag(1);
+			break;
+		case worst_fit:
+			fragmentation = worst_fit_count_extfrag(1);
+			break;
+	}
+	if(fragmentation != 0){
+		printf("ERROR FRAGMENTION IN CLEARED MEMORY\n");
+	}
+}
 
 
+//assign 10 random sized blocks(less then size 100) then remove 5 blocks(1, 3, 4, 6, 8 )
+//then add 3 more of size 24
+//check frag for sizes 1 25 50 then clear mem
+//repeat 100 times to average results 
+void test_3(void** pointers, enum test_type best_or_worst){
+	int i, j = 0;
+	int random_size;
+	double frag_size1, frag_size25, frag_size50 = 0;
+	for(; i < 50; ++i){
+		printf("%d", i);
+		random_size = rand() % 100 + 1;
+		for(; j < 12; ++j){
+			switch(best_or_worst){
+				case best_fit:
+					pointers[j] = best_fit_alloc(random_size);
+					break;
+				case worst_fit:
+					pointers[j] = worst_fit_alloc(random_size);
+					break;
+			}
+		}
+		switch(best_or_worst){
+			case best_fit:
+				best_fit_dealloc(pointers[0]);
+				best_fit_dealloc(pointers[2]);
+				best_fit_dealloc(pointers[3]);
+				best_fit_dealloc(pointers[5]);
+				best_fit_dealloc(pointers[7]);
+				frag_size1 += best_fit_count_extfrag(1);
+				frag_size25 += best_fit_count_extfrag(25);
+				frag_size50 += best_fit_count_extfrag(50);
+				break;
+			case worst_fit:
+				worst_fit_dealloc(pointers[0]);
+				worst_fit_dealloc(pointers[2]);
+				worst_fit_dealloc(pointers[3]);
+				worst_fit_dealloc(pointers[5]);
+				worst_fit_dealloc(pointers[7]);
+				frag_size1 += worst_fit_count_extfrag(1);
+				frag_size25 += worst_fit_count_extfrag(25);
+				frag_size50 += worst_fit_count_extfrag(50);
+				break;
+		}
+		clear_mem(pointers, 12, best_or_worst);
+	}
+	frag_size1 /= 100;
+	frag_size25 /= 100;
+	frag_size50 /= 100;
+	printf("Test Case 3: fragmentation size 1 %lf fragmantation size 25 %lf fragmantation size 50 %lf\n",frag_size1, frag_size25, frag_size50);
+}
+
+//fill memory with blocks of size 100 
+//deallocate 1 3 4 6
+// allocate 3 random sized between 1 and 100 
+//check frag 1 25 50
+//run 100 times
+void test_4(void** pointers, enum test_type best_or_worst){
+	int number;
+	int random_number;
+	int i = 0; 
+	void* most_recent_alloc;
+	double frag_size1, frag_size25, frag_size50 = 0;
+	for(; i < 50 ; ++i){
+		number = 1;
+		if(best_or_worst == best_fit){
+			pointers[0] = best_fit_alloc(100);
+			most_recent_alloc = pointers[0];
+		}
+		else{
+			pointers[0] = worst_fit_alloc(100);
+			most_recent_alloc = pointers[0];
+		}
+		while(most_recent_alloc != NULL){
+			if(best_or_worst == best_fit){
+				pointers[number] = best_fit_alloc(100);
+				most_recent_alloc = pointers[number];
+				number++;
+			}
+			else{
+				pointers[number] = worst_fit_alloc(100);
+				most_recent_alloc = pointers[number];
+				number++;
+			}
+		}
+		if(best_or_worst == best_fit){
+			best_fit_dealloc(pointers[0]);
+			best_fit_dealloc(pointers[2]);
+			best_fit_dealloc(pointers[3]);
+			best_fit_dealloc(pointers[5]);
+			random_number = rand() % 100 + 1;
+			number++;
+			pointers[number] = best_fit_alloc(random_number);
+			random_number = rand() % 100 + 1;
+			number++;
+			pointers[number] = best_fit_alloc(random_number);
+			random_number = rand() % 100 + 1;
+			number++;
+			pointers[number] = worst_fit_alloc(random_number);	
+			frag_size1 += best_fit_count_extfrag(1);
+			frag_size25 += best_fit_count_extfrag(25);
+			frag_size50 += best_fit_count_extfrag(50);	
+		}
+		else{
+			worst_fit_dealloc(pointers[0]);
+			worst_fit_dealloc(pointers[2]);
+			worst_fit_dealloc(pointers[3]);
+			worst_fit_dealloc(pointers[5]);
+			random_number = rand() % 100 + 1;
+			number++;
+			pointers[number] = worst_fit_alloc(random_number);
+			random_number = rand() % 100 + 1;
+			number++;
+			pointers[number] = worst_fit_alloc(random_number);
+			random_number = rand() % 100 + 1;
+			number++;
+			pointers[number] = worst_fit_alloc(random_number);
+			frag_size1 += worst_fit_count_extfrag(1);
+			frag_size25 += worst_fit_count_extfrag(25);
+			frag_size50 += worst_fit_count_extfrag(50);
+		}
+		clear_mem(pointers, number, best_or_worst);
+	}
+	frag_size1 /= 100;
+	frag_size25 /= 100;
+	frag_size50 /= 100;
+	printf("Test Case 4: fragmentation size 1 %lf fragmantation size 25 %lf fragmantation size 50 %lf\n",frag_size1, frag_size25, frag_size50);
+}
+
+// fill with size 4
+// deallocte 10 places between 1 and 100
+// allocate 10 blocks of size 2
+//check frag 1 25 50
+//repeat 100 times
+void test_5(void** pointers, enum test_type best_or_worst){
+	int number;
+	int random_number;
+	int i = 0; 
+	void* most_recent_alloc;
+	double frag_size1, frag_size25, frag_size50 = 0;
+	for(; i < 50 ; ++i){
+		number = 1;
+		if(best_or_worst == best_fit){
+			pointers[0] = best_fit_alloc(2);
+			most_recent_alloc = pointers[0];
+		}
+		else{
+			pointers[0] = worst_fit_alloc(2);
+			most_recent_alloc = pointers[0];
+		}
+		while(most_recent_alloc != NULL){
+			if(best_or_worst == best_fit){
+				pointers[number] = best_fit_alloc(2);
+				most_recent_alloc = pointers[number];
+				number++;
+			}
+			else{
+				pointers[number] = worst_fit_alloc(2);
+				most_recent_alloc = pointers[number];
+				number++;
+			}
+		}
+		if(best_or_worst == best_fit){
+			best_fit_dealloc(pointers[0]);
+			best_fit_dealloc(pointers[2]);
+			best_fit_dealloc(pointers[3]);
+			best_fit_dealloc(pointers[5]);
+			best_fit_dealloc(pointers[6]);
+			best_fit_dealloc(pointers[7]);
+			best_fit_dealloc(pointers[55]);
+			best_fit_dealloc(pointers[67]);
+			best_fit_dealloc(pointers[68]);
+			best_fit_dealloc(pointers[69]);
+			random_number = rand() % 5 + 1;
+			number++;
+			pointers[number] = best_fit_alloc(random_number);
+			random_number = rand() % 5 + 1;
+			number++;
+			pointers[number] = best_fit_alloc(random_number);
+			random_number = rand() % 5 + 1;
+			number++;
+			pointers[number] = worst_fit_alloc(random_number);	
+			frag_size1 += best_fit_count_extfrag(1);
+			frag_size25 += best_fit_count_extfrag(25);
+			frag_size50 += best_fit_count_extfrag(50);	
+		}
+		else{
+			best_fit_dealloc(pointers[0]);
+			best_fit_dealloc(pointers[2]);
+			best_fit_dealloc(pointers[3]);
+			best_fit_dealloc(pointers[5]);
+			best_fit_dealloc(pointers[6]);
+			best_fit_dealloc(pointers[7]);
+			best_fit_dealloc(pointers[55]);
+			best_fit_dealloc(pointers[67]);
+			best_fit_dealloc(pointers[68]);
+			best_fit_dealloc(pointers[69]);
+			random_number = rand() % 5 + 1;
+			number++;
+			pointers[number] = worst_fit_alloc(random_number);
+			random_number = rand() % 5 + 1;
+			number++;
+			pointers[number] = worst_fit_alloc(random_number);
+			random_number = rand() % 5 + 1;
+			number++;
+			pointers[number] = worst_fit_alloc(random_number);
+			frag_size1 += worst_fit_count_extfrag(1);
+			frag_size25 += worst_fit_count_extfrag(25);
+			frag_size50 += worst_fit_count_extfrag(50);
+		}
+		clear_mem(pointers, number, best_or_worst);
+	}
+	frag_size1 /= 100;
+	frag_size25 /= 100;
+	frag_size50 /= 100;
+	printf("Test Case 5: fragmentation size 1 %lf fragmantation size 25 %lf fragmantation size 50 %lf\n",frag_size1, frag_size25, frag_size50);
+}
+
+
+//fill with size 25 
+//deallocate 10 place between 1 and 40
+//allocate 3 blocks of size 1 - 25
+//check frag 1 25 50
+//repeat 100 times 
+void test_6(void** pointers, enum test_type best_or_worst){
+	int number;
+	int random_number;
+	int i, j = 0; 
+	void* most_recent_alloc;
+	double frag_size1, frag_size25, frag_size50 = 0;
+}
+
+//allocate 15 random sized blocks
+//deallocate them
+//repeat 100 times 
+//time how long it takes
+void test_7(void** pointers, enum test_type best_or_worst){
+	double g_time[2];
+	int i, j = 0;
+	int random_size = 0;
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
+	for(; i < 100; ++i){
+		for(; j < 15; ++j){
+			random_size = rand() % 100 + 1;
+			switch(best_or_worst){
+				case best_fit:
+					pointers[j] = best_fit_alloc(random_size);
+					break;
+				case worst_fit:
+					pointers [j] = worst_fit_alloc(random_size);
+					break;
+			}
+		}
+		for(j = 0 ; j < 15; ++j){
+			switch(best_or_worst){
+				case best_fit:
+					best_fit_dealloc(pointers[j]);
+					break;
+
+				case worst_fit:
+					worst_fit_dealloc(pointers[j]);
+			}
+		}
+	}
+
+	gettimeofday(&tv, NULL);
+    g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
+	printf("System execution time: %.6lf seconds\n", \
+            g_time[1] - g_time[0]);
+}
 
 int main(int argc, char *argv[])
 {
-	// BASIC BEST TEST
-	best_fit_memory_init(2000);
-	best_fit_alloc(10);
-	printf("Attempting a print mem \n");
-	print_mem(0);
-	void* ptr3 = best_fit_alloc(20);
-	print_mem(0);
-	void* ptr2 = best_fit_alloc(30);
-	print_mem(0);
-	best_fit_alloc(31);
-	print_mem(0);
-	void* ptr = best_fit_alloc(521);
-	print_mem(0);
-	best_fit_alloc(33);
-	print_mem(0);
-	printf("Attempt to deallocate pointer \n");
-	best_fit_dealloc(ptr);
-	print_mem(0);
-	best_fit_dealloc(ptr2);
-	print_mem(0);
-	best_fit_alloc(69);
-	print_mem(0);
-	void* ptr4 = best_fit_alloc(12);
-	print_mem(0);
-	best_fit_dealloc(ptr3);
-	print_mem(0);
-	best_fit_dealloc(ptr4);
-	print_mem(0);
-	int external = best_fit_count_extfrag(400);
-	printf("Defragged spaces is %d \n",external);
-
-	// BASIC WORST TEST
-	// worst_fit_memory_init(1024);
-	// worst_fit_alloc(10);
-	// print_mem(1);
-	// void* ptr3 = worst_fit_alloc(20);
-	// print_mem(1);
-	// void* ptr2 = worst_fit_alloc(30);
-	// print_mem(1);
-	// worst_fit_alloc(31);
-	// print_mem(1);
-	// void* ptr = worst_fit_alloc(521);
-	// print_mem(1);
-	// worst_fit_alloc(33);
-	// print_mem(1);
-	// printf("Attempt to deallocate pointer \n");
-	// worst_fit_dealloc(ptr);
-	// print_mem(1);
-	// worst_fit_dealloc(ptr2);
-	// print_mem(1);
-	// worst_fit_alloc(69);
-	// print_mem(1);
-	// void* ptr4 = worst_fit_alloc(12);
-	// print_mem(1);
-	// worst_fit_dealloc(ptr3);
-	// print_mem(1);
-	// worst_fit_dealloc(ptr4);
-	// print_mem(1);
-	// int external = worst_fit_count_extfrag(400);
-	// printf("Defragged spaces is %d \n",external);
-
-
-	// int num = 0;
-	// int algo = 0; // default algorithm to test is best fit  
-	// void *p, *q;
+	 int algo = 0; // default algorithm to test is best fit  
+	 void** pointers = malloc(25 * sizeof(void*));
+	 enum test_type best_or_worst;
 
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s <0/1>. 0 for best fit and 1 for worst fit \n", argv[0]);
@@ -96,35 +385,23 @@ int main(int argc, char *argv[])
 	}
 	
 	if ( algo == 0 ) {
-		best_fit_memory_init(1024);	// initizae 1KB, best fit
-
-		p = best_fit_alloc(8);		// allocate 8B
-		printf("best fit: p=%p\n", p);
-		if ( p != NULL ) {
-			best_fit_dealloc(p);	
-		}
-		num = best_fit_count_extfrag(4);
+		best_or_worst = best_fit;
+		best_fit_memory_init(2048);	// initizae 2KB, best fit
 	} else if ( algo == 1 ) {
-
-		worst_fit_memory_init(1024);	// initizae 1KB, worst fit
-
-		q = worst_fit_alloc(8);		// allocate 8B
-		printf("worst fit: q=%p\n", q);
-		if ( q != NULL ) {
-			worst_fit_dealloc(q);	
-		}
-		num = worst_fit_count_extfrag(4);
-
-		//////Test 1
-		
-
-
+		best_or_worst = worst_fit;
+		worst_fit_memory_init(2048);	// initizae 2KB, worst fit
+		//num = worst_fit_count_extfrag(4);	
 	} else {
 		fprintf(stderr, "Should not reach here!\n");
 		exit(1);
 	}
 
-	printf("num = %d\n", num);
+	test_1(pointers, best_or_worst);
+	test_2(pointers, best_or_worst);
+	test_3(pointers, best_or_worst);
+	test_4(pointers, best_or_worst);
+	test_5(pointers, best_or_worst);
+	test_7(pointers, best_or_worst);
 
 	return 0;
 }
--- starter/mem.c
+++ starter/mem.c
@@ -35,7 +35,7 @@ void worst_fit_create_node(node_t* node, node_t* new_node,size_t size){
 	head_t* head = worst_fit_mem_start;
 	node->filled = 1;
 	new_node -> next_node = node -> next_node;
-	printf("current node is %d and next node is %d \n",node,new_node);
+///	printf("current node is %d and next node is %d \n",node,new_node);
 	new_node -> prev_node = node;
 	new_node -> filled = 0;
 	if(node -> next_node == NULL){
@@ -43,7 +43,7 @@ void worst_fit_create_node(node_t* node, node_t* new_node,size_t size){
 	}
 	else{
 		new_node ->next_node->prev_node = new_node;
-		printf("Current node is %d and next node is %d \n",new_node,new_node->next_node);
+//		printf("Current node is %d and next node is %d \n",new_node,new_node->next_node);
 		// new_node -> mem_size = (new_node -> next_node) - ((intptr_t)new_node + sizeof(node_t));	
 		new_node -> mem_size = (node->mem_size) - (size + sizeof(node_t));		
 	}	
@@ -57,7 +57,7 @@ void best_fit_create_node(node_t* node, node_t* new_node,size_t size){
 	head_t* head = best_fit_mem_start;
 	node->filled = 1;
 	new_node -> next_node = node -> next_node;
-	printf("current node is %d and next node is %d \n",node,new_node);
+//	printf("current node is %d and next node is %d \n",node,new_node);
 	new_node -> prev_node = node;
 	new_node -> filled = 0;
 	if(node -> next_node == NULL){
@@ -65,7 +65,7 @@ void best_fit_create_node(node_t* node, node_t* new_node,size_t size){
 	}
 	else{
 		new_node ->next_node->prev_node = new_node;
-		printf("Current node is %d and next node is %d \n",new_node,new_node->next_node);
+//		printf("Current node is %d and next node is %d \n",new_node,new_node->next_node);
 		new_node -> mem_size = (new_node -> next_node) - ((intptr_t)new_node + sizeof(node_t));	
 		new_node -> mem_size = (node->mem_size) - (size + sizeof(node_t));		
 	}	
@@ -77,7 +77,7 @@ void best_fit_create_node(node_t* node, node_t* new_node,size_t size){
 
 void combine_nodes(node_t* node, node_t* second_node){
 	node->next_node = second_node->next_node;
-	node->next_node->prev_node = second_node;
+//	node->next_node->prev_node = second_node;
 	node->mem_size = (intptr_t)(node->mem_size) + sizeof(node_t) + second_node->mem_size;
 }
 
@@ -90,7 +90,7 @@ void print_mem(int mode){ // 1 is worst 0 is best
 	node_t* itr = head->first_node;
 	printf("...PRINTING MEMORY... \n");
 	while(itr != NULL){
-		printf("Current mem_start is %d is filled: %d and mem_size: %d next_node is: %d \n", itr->mem_start,itr->filled,itr->mem_size,itr->next_node);
+//		printf("Current mem_start is %d is filled: %d and mem_size: %d next_node is: %d \n", itr->mem_start,itr->filled,itr->mem_size,itr->next_node);
 		itr = itr->next_node;
 	}
 }
@@ -100,18 +100,18 @@ void print_mem(int mode){ // 1 is worst 0 is best
 /* memory initializer */
 int best_fit_memory_init(size_t size)
 {
-	printf(" \n**************** START BEST INIT **************\n \n");
+	//printf(" \n**************** START BEST INIT **************\n \n");
 	if(size < (sizeof(node_t) + sizeof(head_t))){
 		printf("ERROR: Size too small");
 		return -1;
 	}
 	
 	best_fit_mem_start = malloc(size);
-	printf("Start of memory is located at %d \n",best_fit_mem_start);
+	//printf("Start of memory is located at %d \n",best_fit_mem_start);
 	head_t* head = best_fit_mem_start;
 	head->first_node = sizeof(head_t) + best_fit_mem_start;
 	head->end_of_mem = best_fit_mem_start + size;
-	printf("End of memory is located at %d \n",head->end_of_mem);
+	//printf("End of memory is located at %d \n",head->end_of_mem);
 	node_t* first_node = head->first_node;
 	first_node -> next_node = NULL;
 	first_node -> prev_node = NULL;
@@ -119,26 +119,26 @@ int best_fit_memory_init(size_t size)
 	first_node -> mem_size = size - (sizeof(head_t) + sizeof(node_t));
 	first_node -> filled = 0;
 
-	printf(" \nFirst memory location available at %x with space %d\n",first_node->mem_start,first_node->mem_size);
-	printf("Size of head %d and size of node %d \n",sizeof(head_t),sizeof(node_t));
-	printf(" \n**************** FINISH INIT **************\n \n");
+	//printf(" \nFirst memory location available at %x with space %d\n",first_node->mem_start,first_node->mem_size);
+	//printf("Size of head %d and size of node %d \n",sizeof(head_t),sizeof(node_t));
+	//printf(" \n**************** FINISH INIT **************\n \n");
 	return 0;
 }
 
 int worst_fit_memory_init(size_t size)
 {
-	printf(" \n**************** START WORST INIT **************\n \n");
+	//printf(" \n**************** START WORST INIT **************\n \n");
 	if(size < (sizeof(node_t) + sizeof(head_t))){
 		printf("ERROR: Size too small");
 		return -1;
 	}
 	
 	worst_fit_mem_start = malloc(size);
-	printf("Start of memory is located at %d \n",worst_fit_mem_start);
+	//printf("Start of memory is located at %d \n",worst_fit_mem_start);
 	head_t* head = worst_fit_mem_start;
 	head->first_node = sizeof(head_t) + worst_fit_mem_start;
 	head->end_of_mem = worst_fit_mem_start + size;
-	printf("End of memory is located at %d \n",head->end_of_mem);
+	//printf("End of memory is located at %d \n",head->end_of_mem);
 	node_t* first_node = head->first_node;
 	first_node -> next_node = NULL;
 	first_node -> prev_node = NULL;
@@ -146,9 +146,9 @@ int worst_fit_memory_init(size_t size)
 	first_node -> mem_size = size - (sizeof(head_t) + sizeof(node_t));
 	first_node -> filled = 0;
 
-	printf(" \nFirst memory location available at %x with space %d\n",first_node->mem_start,first_node->mem_size);
-	printf("Size of head %d and size of node %d \n",sizeof(head_t),sizeof(node_t));
-	printf(" \n**************** FINISH INIT **************\n \n");
+	//printf(" \nFirst memory location available at %x with space %d\n",first_node->mem_start,first_node->mem_size);
+	//printf("Size of head %d and size of node %d \n",sizeof(head_t),sizeof(node_t));
+	//printf(" \n**************** FINISH INIT **************\n \n");
 	return 0;
 
 }
@@ -156,7 +156,7 @@ int worst_fit_memory_init(size_t size)
 /* memory allocators */
 void *best_fit_alloc(size_t size)
 {
-	printf("===================== CALL OF BEST ALLOC =================== \n");
+	//printf("===================== CALL OF BEST ALLOC =================== \n");
 	node_t* new_node;
 	node_t* smallest_available = NULL;
 	size_t smallest_size = 0;
@@ -188,8 +188,8 @@ void *best_fit_alloc(size_t size)
 		new_node->mem_size = head->end_of_mem - new_node->mem_start;
 		new_node->filled = 0;
 
-		printf("Created first node %d with size %d and with the next node occuring at %d \n",(unsigned long)itr,size,(unsigned long)(itr->next_node));
-		printf("===================== FINISH ALLOC =================== \n");
+	//	printf("Created first node %d with size %d and with the next node occuring at %d \n",(unsigned long)itr,size,(unsigned long)(itr->next_node));
+	//	printf("===================== FINISH ALLOC =================== \n");
 		return itr;
 	}
 	// Loop to search for smallest available memory locations
@@ -216,10 +216,10 @@ void *best_fit_alloc(size_t size)
 		if(offset_four_byte == 4){
 			offset_four_byte = 0;
 		}
-		printf("Creating new node with size %d \n",size + offset_four_byte);
+	//	printf("Creating new node with size %d \n",size + offset_four_byte);
 		new_node = (intptr_t)new_node + offset_four_byte; 
 		best_fit_create_node(itr,new_node,size+offset_four_byte);
-		printf("===================== FINISH ALLOC =================== \n");
+	//	printf("===================== FINISH ALLOC =================== \n");
 		return itr->mem_start;
 	}
 	printf("ERROR: Unable to find space for request of size %d \n",size);
@@ -229,7 +229,7 @@ void *best_fit_alloc(size_t size)
 
 void *worst_fit_alloc(size_t size)
 {
-	printf("===================== CALL OF WORST ALLOC =================== \n");
+	//printf("===================== CALL OF WORST ALLOC =================== \n");
 	node_t* new_node;
 	node_t* largest_available = 0;
 	size_t largest_size = 0;
@@ -251,7 +251,7 @@ void *worst_fit_alloc(size_t size)
 		itr->mem_size = size + offset_four_byte;
 		itr->filled = 1;
 		if(((intptr_t)(head->end_of_mem) - (intptr_t)(itr->next_node) < sizeof(node_t))){
-			printf("ERROR: Not enough space to create second node \n");
+	//		printf("ERROR: Not enough space to create second node \n");
 			return itr;
 		}
 		new_node = itr->next_node;
@@ -261,8 +261,8 @@ void *worst_fit_alloc(size_t size)
 		new_node->mem_size = head->end_of_mem - new_node->mem_start;
 		new_node->filled = 0;
 
-		printf("Created first node %d with size %d and with the next node occuring at %d \n",(unsigned long)itr,size,(unsigned long)(itr->next_node));
-		printf("===================== FINISH ALLOC =================== \n");
+	//	printf("Created first node %d with size %d and with the next node occuring at %d \n",(unsigned long)itr,size,(unsigned long)(itr->next_node));
+	//	printf("===================== FINISH ALLOC =================== \n");
 		return itr;
 	}
 	// Loop to search for largest available memory location
@@ -278,51 +278,55 @@ void *worst_fit_alloc(size_t size)
 	if(largest_size > size + sizeof(node_t)){
 		itr = largest_available; //Focus itr onto largest available node
 		node_t* new_node = (intptr_t)itr + sizeof(node_t) + size;
-		printf("Creating new node with size %d \n",size);
+	//	printf("Creating new node with size %d \n",size);
 		int offset_four_byte = 4 - (size % 4); // New node has already been previous node + size + size of node (so now see if we need to add any offset so next will start byte aligned)
 		if(offset_four_byte == 4){
 			offset_four_byte = 0;
 		}
 		new_node = (intptr_t)new_node + offset_four_byte; 
 		worst_fit_create_node(itr,new_node,size+offset_four_byte);
-		printf("===================== FINISH ALLOC =================== \n");
+	//	printf("===================== FINISH ALLOC =================== \n");
 		return itr->mem_start;
 	}
-	printf("ERROR: Unable to find space for request of size %d \n",size);
+	//printf("ERROR: Unable to find space for request of size %d \n",size);
 	return NULL;
 }
 
 /* memory de-allocator */
 void best_fit_dealloc(void *ptr) 
 {
-	printf("===================== START DEALLOC =================== \n");
+	//printf("===================== START DEALLOC =================== \n");
 	head_t* head = best_fit_mem_start;
 	node_t* itr = head->first_node;
 	if((ptr < head) || (ptr > head->end_of_mem)){
-		printf("ERROR: Requested deallocation out of memory range \n");
+		printf("ERROR: Requested deallocation out of memory range, %x, %x, %x \n", ptr, head, head->end_of_mem);
 		return;
 	}
 
 	while(itr != NULL){
 		if(itr->mem_start == ptr){
-			printf("SUCCESS: Deallocated ptr %d \n",ptr);
+			//printf("SUCCESS: Deallocated ptr %d \n",ptr);
+			if (itr == NULL) printf("badd\n"); 
+			//printf("val: %d\n", itr->filled); 
 			itr->filled = 0;
+			//printf("before if statement");
 			if((itr->filled == 0) && (itr->next_node->filled == 0)){ // Checking for consecutive unfilled memory spaces to join
+			//	printf("before combine");
 				combine_nodes(itr,itr->next_node);
-				printf("SUCCESS: Combined two empty adjacent spaces \n");
+			//	printf("SUCCESS: Combined two empty adjacent spaces \n");
 			}
-			printf("===================== FINISH DEALLOC =================== \n");
+			//printf("===================== FINISH DEALLOC =================== \n");
 			return;
 		}
 		itr = itr->next_node;
 	}
-	printf("===================== FINISH DEALLOC =================== \n");	
+	//printf("===================== FINISH DEALLOC =================== \n");	
 	return;
 }
 
 void worst_fit_dealloc(void *ptr) 
 {
-	printf("===================== START DEALLOC =================== \n");
+	//printf("===================== START DEALLOC =================== \n");
 	head_t* head = worst_fit_mem_start;
 	node_t* itr = head->first_node;
 	if((ptr < head) || (ptr > head->end_of_mem)){
@@ -332,18 +336,18 @@ void worst_fit_dealloc(void *ptr)
 
 	while(itr != NULL){
 		if(itr->mem_start == ptr){
-			printf("SUCCESS: Deallocated ptr %d \n",ptr);
+	//		printf("SUCCESS: Deallocated ptr %d \n",ptr);
 			itr->filled = 0;
 			if((itr->filled == 0) && (itr->next_node->filled == 0)){ // Checking for consecutive unfilled memory spaces to join
 				combine_nodes(itr,itr->next_node);
-				printf("SUCCESS: Combined two empty adjacent spaces \n");
+	//			printf("SUCCESS: Combined two empty adjacent spaces \n");
 			}
-			printf("===================== FINISH DEALLOC =================== \n");
+	//		printf("===================== FINISH DEALLOC =================== \n");
 			return;
 		}
 		itr = itr->next_node;
 	}
-	printf("===================== FINISH DEALLOC =================== \n");	
+	//printf("===================== FINISH DEALLOC =================== \n");	
 	return;
 }
 
